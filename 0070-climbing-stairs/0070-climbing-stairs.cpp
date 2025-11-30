67ed5d3fb7d0b59c6f2b276f70fb57c388dcd7be
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2025-11-30 13:36:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.5%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 1) return 1;
+
+        int a = 1, b = 1; 
+        int c;
+
+        for(int i = 2; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
