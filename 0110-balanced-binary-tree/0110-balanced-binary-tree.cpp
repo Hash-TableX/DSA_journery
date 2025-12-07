1e473a7cfa16b980332f59b6b63d8dd0ac78b5e6
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2025-12-07 20:52:39 +0530

    Time: 0 ms (100%), Space: 22.9 MB (83.97%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,22 +12,27 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return height(root)!=-1;
+        return height(root) != -1;
     }
+
     int height(TreeNode* root){
         if(root==NULL){
-            return 0;
+            return -0;
         }
+
         int lh = height(root->left);
-            if(lh==-1){
-                return -1;
-            }
-        int rh = height(root->right);
-            if(rh==-1){
-                return -1;
-            }
-        if(abs(lh-rh)>1)
+        if(lh==-1){
             return -1;
-        return max(lh,rh)+1;
+        }
+        int rh = height(root->right);
+        if(rh==-1){
+            return -1;
+        }
+
+        if(abs(lh-rh)>1){
+            return -1;
+        }
+
+        return 1+max(lh,rh);
     }
 };
