f4bb10c7a1b32ff129e8a7e627ec8af5e7482fb3
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2025-12-19 22:51:02 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int zeros,ones,twos;
+        zeros=ones=twos=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                zeros++;
+            }
+            else if(nums[i]==1){
+                ones++;
+            }
+            else{
+                twos++;
+            }
+        }
+
+        int i=0;
+        while(zeros!=0){
+            nums[i]=0;
+            i++;
+            zeros--;
+        }
+
+        while(ones!=0){
+            nums[i]=1;
+            i++;
+            ones--;
+        }
+
+        while(twos!=0){
+            nums[i]=2;
+            i++;
+            twos--;
+        }
     }
 };
