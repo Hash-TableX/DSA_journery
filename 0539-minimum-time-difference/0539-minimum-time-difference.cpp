2fdb6927396281645a23b582865fe6d4a9c49ea6
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2025-12-23 09:36:21 +0530

    Time: 3 ms (67.6%), Space: 17.9 MB (81.4%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -5,22 +5,24 @@ public:
 
         for(int i=0;i<timePoints.size();i++){
             string curr = timePoints[i];
-            int hours = stoi(curr.substr(0,2));
+
+            int hour = stoi(curr.substr(0,2));
             int min = stoi(curr.substr(3,2));
-            int totalMin = hours*60+min;
+            int totalMin = hour*60+min;
             minutes.push_back(totalMin);
         }
 
-        sort(minutes.begin(),minutes.end());
 
+        sort(minutes.begin(),minutes.end());
         int mini = INT_MAX;
         int n = minutes.size();
+
         for(int i=0;i<n-1;i++){
             int diff = minutes[i+1] - minutes[i];
-            mini = min(mini,diff);
+            mini = min(mini,diff); 
         }
 
-        int lastDiff = (minutes[0]+1440) - minutes[n-1];
+        int lastDiff = (1440+minutes[0]) - minutes[n-1];
         mini = min(mini,lastDiff);
         return mini;
     }
