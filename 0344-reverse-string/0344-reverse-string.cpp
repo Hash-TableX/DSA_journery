6a0e7f15d65e719013f1eb6ab0f1a2068b6e06e7
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2026-02-16 21:04:29 +0530

    Time: 0 ms (100%), Space: 27.3 MB (15.34%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-       int left=0;
-       int right=s.size()-1;
+        int n = s.size();
+        int left=0;
+        int right=n-1;
 
-       while(left<right)
-       {
-        swap(s[left], s[right]);
-        left++;
-        right--;
-       } 
+        while(left<right){
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        }   
     }
 };
