3a25dea99b3c1d42c9107cbb1e992efd9971e063
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2026-02-20 17:10:17 +0530

    Time: 0 ms (100%), Space: 29.6 MB (67.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        int l = k%n;
+        k=k%n;
 
         reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+l);
-        reverse(nums.begin()+l,nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
