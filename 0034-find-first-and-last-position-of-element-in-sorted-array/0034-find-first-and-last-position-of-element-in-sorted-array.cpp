485aa7b9b7f39d07000669433cd6b0640dcd808a
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2026-03-10 05:35:58 +0530

    Time: 0 ms (100%), Space: 17.6 MB (19.4%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,41 +2,21 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int low=0;
-        int high=n-1;
-        int mid;
         int first=-1;
         int last=-1;
 
-        while(low<=high){
-            mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                first=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                first=i;
+                break;
             }
         }
-        low=0;
-        high=n-1;
-
-        while(low<=high){
-            mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                last=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]==target){
+                last=i;
+                break;
             }
         }
-        return {first,last};
+    return {first,last};
     }
 };
