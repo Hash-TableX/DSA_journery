9e92fb40a5accd115b03b788d78238809af80840
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2026-03-13 00:13:24 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80.28%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                return i;
+        int n = nums.size();
+        int low=0;
+        int high=n-1;
+        int mid;
+
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return -1;
