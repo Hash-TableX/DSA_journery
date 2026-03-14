edbfa465edf52065e1600f39f2eab7eb37f293d6
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2026-03-14 17:21:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -1,26 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int cnt = 0;
+       ListNode* slow=head;
+       ListNode* fast=head;
 
-        // Count total nodes
-        while(temp != NULL){
-            cnt++;
-            temp = temp->next;
-        }
-
-        int midNode = (cnt/2) + 1;
-
-        temp = head;
-        while(temp != NULL){
-            midNode--;
-            if(midNode == 0){
-                break;
-            }
-            temp = temp->next;
-        }
-
-        return temp;
+       while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+       } 
+       return slow;
     }
 };
