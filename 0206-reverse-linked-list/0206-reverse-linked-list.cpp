a1f3f09ebe888ae48ac908f88971acfc5d9b7c6d
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2026-03-14 18:23:25 +0530

    Time: 0 ms (100%), Space: 13.8 MB (9.05%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,15 +11,18 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-
-        while(curr != NULL){
-            ListNode* next = curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
+        ListNode* temp = head;
+        stack<int>st;
+        while(temp!=NULL){
+            st.push(temp->val);
+            temp=temp->next;
         }
-        return prev;
+        temp=head;
+        while(temp!=NULL){
+            temp->val=st.top();
+            st.pop();
+            temp=temp->next;
+        }
+        return head;
     }
 };
