ef71a583acf557b746494882ef9ea0d77eef4fa6
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2026-03-23 01:22:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<char>st;
-        string result = "";
+        stack<int>st;
+        string result="";
 
         for(int i=0;i<s.length();i++){
             if(s[i]=='('){
                 if(!st.empty()){
-                    result = result+s[i];
+                    result = result + s[i];
                 }
                 st.push(s[i]);
             }
             else{
                 st.pop();
                 if(!st.empty()){
-                    result = result+s[i];
+                    result = result + s[i];
                 }
             }
         }
