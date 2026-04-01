a93ca75170c7a4420f726338a2113b855662b674
Harsh Kashyap <148344777+Hash-TableX@users.noreply.github.com>
2026-04-01 18:57:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,11 +3,11 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        int i=0;
-        for(int j=1;j<n;j++){
-            if(nums[j]!=0){
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
                 swap(nums[i],nums[j]);
-                i++;
+                j++;
             }
         }
     }
